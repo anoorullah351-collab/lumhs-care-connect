80badbb4477e4af261cd0e8b9157885d40eada13
anoorullah351-collab <anoorullah351@gmail.com>
2026-05-31 19:26:01 +0500

    Add android build files

---
+++ android/app/src/main/java/com/noorullah/wardfollowup/MainActivity.java
@@ -0,0 +1,5 @@
+package com.noorullah.wardfollowup;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.noorullah.wardfollowup',
+  appName: 'WardFollowUp',
+  webDir: 'dist/client',
+};
+
+export default config;
